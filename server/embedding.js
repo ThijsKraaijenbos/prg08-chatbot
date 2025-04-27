cc81a3214b48c3eebe9a0c91205f9072131b3933
Thijs Kraaijenbos <47988622+ThijsKraaijenbos@users.noreply.github.com>
2025-04-27 15:25:46 +0200

    Created behavior guide PDF embeddig (114 pages). Might be tweaked later.

--- server/embedding.js
+++ server/embedding.js
@@ -1,8 +1,8 @@
 import { AzureChatOpenAI, AzureOpenAIEmbeddings } from "@langchain/openai";
 import { TextLoader } from "langchain/document_loaders/fs/text";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-// import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { FaissStore } from "@langchain/community/vectorstores/faiss";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf"
 
 const model = new AzureChatOpenAI({temperature: 1});
 
@@ -16,13 +16,15 @@ console.log(vectorTest)
 let vectorStore
 
 async function createVectorstore() {
-    const loader = new TextLoader("./public/story.txt");
+    const loader = new PDFLoader("./public/behaviour-guide-2021.pdf");
     const docs = await loader.load();
-    const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 300, chunkOverlap: 50 });
+
+    const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000, chunkOverlap: 250 });
     const splitDocs = await textSplitter.splitDocuments(docs);
     console.log(`Document split into ${splitDocs.length} chunks. Now saving into vector store`);
+
     vectorStore = await FaissStore.fromDocuments(splitDocs, embeddings);
-    await vectorStore.save("sillycatdb"); // geef hier de naam van de directory waar je de data gaat opslaan
+    await vectorStore.save("cat-behavior-db"); // geef hier de naam van de directory waar je de data gaat opslaan
     console.log("vector store created")
 }
 
@@ -39,4 +41,4 @@ async function askQuestion(prompt){
 
 
 await createVectorstore()
-await askQuestion("What is the full name of the main character?")
+await askQuestion("Give me the bullet points of the \"Development of play\" paragraph")
